e998af2482362b16d3e449ab0c56a6a1b45334b5
nathanchen0 <nathanchen215@gmail.com>
2025-11-21 18:45:25 -0500

    Add turtle graphics polygon drawing script
    This script uses the turtle graphics library to draw a polygon based on user input for the number of sides and length of each side.

---
+++ week6/polygon.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.color("red")
+turtle.speed(1)
+
+sides = int(input("Enter the number of sides for the polygon: "))
+length = int(input("Enter the length of each side: "))
+angle = (sides-2)*180/sides
+
+for i in range(sides):
+    turtle.forward(length)
+    turtle.left(180 - angle)
